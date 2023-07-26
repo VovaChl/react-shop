1544a8d89dbbec10ca6cccf91424d540a6508f4c
VovaChl <115529520+VovaChl@users.noreply.github.com>
2023-07-26 14:35:54 +0500

    Update GoodsList.jsx

--- src/components/GoodsList.jsx
+++ src/components/GoodsList.jsx
@@ -6,9 +6,12 @@ function GoodsList(props) {
     if (!goods.length) return <h3>Nothing there</h3>
 
     return <div className="goods">
-            {goods.map(good => (
-                <GoodsItem key={good.mainId} {...good} addToOrder={addToOrder} />
-            ))}
+            {goods.map(good => {
+                    if (good.mainId && good.displayAssets.length && good.displayName) {
+                        return <GoodsItem key={good.mainId} {...good} addToOrder={addToOrder} />
+                    }
+                    return false;
+                })}
     </div>
 }
 
